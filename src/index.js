2c12d9f68f8adb71dd4e4215dadf3633d9ff3a56
Kyle <k.yasumiishi@gmail.com>
2018-07-28 14:18:30 -0700

    Added redux store to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(allReducers);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root");
+// );
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
 registerServiceWorker();
